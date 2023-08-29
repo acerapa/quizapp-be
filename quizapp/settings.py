31ef334c7555002b3b96cb5f4f0c528b3c9197b1
haparece <harvey@fulloutsoftware.com>
2023-08-29 12:35:47 +0800

    Hot fixes

--- quizapp/settings.py
+++ quizapp/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = 'django-insecure-py=))en+eat!un#%=xwako+mngvbw#0x(l1bymvc6k!61&x%d$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', "False").lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS',[]).split(' ')
+print(DEBUG)
+
+ALLOWED_HOSTS = [] if DEBUG else os.environ.get('ALLOWED_HOSTS',[]) if os.environ.get('ALLOWED_HOSTS',[]) else []
 
 # Application definition
 
@@ -159,7 +161,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
